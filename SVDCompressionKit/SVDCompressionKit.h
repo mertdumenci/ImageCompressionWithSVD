3737ab575227fa7e0caba7a729691fa45b168f4b
Mert Dümenci <mert@dumenci.me>
2017-03-28 23:45:35 -0400

    Initial Commit

---
+++ SVDCompressionKit/SVDCompressionKit.h
@@ -0,0 +1,19 @@
+//
+//  SVDCompressionKit.h
+//  SVDCompressionKit
+//
+//  Created by Mert Dümenci on 3/28/17.
+//  Copyright © 2017 Mert Dümenci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVDCompressionKit.
+FOUNDATION_EXPORT double SVDCompressionKitVersionNumber;
+
+//! Project version string for SVDCompressionKit.
+FOUNDATION_EXPORT const unsigned char SVDCompressionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVDCompressionKit/PublicHeader.h>
+
+
